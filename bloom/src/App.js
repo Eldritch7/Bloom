717862d00321e6803db17d3f8d677b9aad682abf
Sara <parakeet_22@yahoo.com>
2022-12-27 18:10:40 -0500

    click through an image works

--- bloom/src/App.js
+++ bloom/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import react, {useState} from "react";
 //images
 import one from './images/1.png';
 import two from './images/2.png';
@@ -7,10 +8,15 @@ import three from './images/3.png';
 import four from './images/4.png';
 
 function App() {
+  const [image, setImage] = useState(0);
+  const images = [one, two, three, four];
+  const loopImage = function() {
+    setImage(image === images.length -1 ? 0 : image +1 );
+  }
   return (
     <div className="App">
       <header className="Bloom-Header">
-        <img src={four} className="image"></img>
+        <img src={images[image]} onClick = {loopImage} className="image"></img>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
