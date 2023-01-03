320369788581332e054ff6c5ba79e0294d106f90
Sara <parakeet_22@yahoo.com>
2023-01-02 19:48:30 -0500

    rearange app component

--- bloom/src/components/App.js
+++ bloom/src/components/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import react, {useState} from "react";
+
+import "./App.css";
+import react, { useState } from "react";
 //images
-import one from './images/1.png';
-import two from './images/2.png';
-import three from './images/3.png';
-import four from './images/4.png';
+import one from "../images/1.png";
+import two from "../images/2.png";
+import three from "../images/3.png";
+import four from "../images/4.png";
 
 function App() {
   const [image, setImage] = useState(0);
   const images = [one, two, three, four];
-  const loopImage = function() {
-    setImage(image === images.length -1 ? 0 : image +1 );
-  }
+  const loopImage = function () {
+    setImage(image === images.length - 1 ? 0 : image + 1);
+  };
   return (
     <div className="App">
       <header className="Bloom-Header">
-        <img src={images[image]} onClick = {loopImage} className="image"></img>
-      {/* <header className="App-header">
+        <img src={images[image]} onClick={loopImage} className="image"></img>
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
