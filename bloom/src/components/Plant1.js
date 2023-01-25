aedb54dcdba6d9acbe910a2800e0a63c74cc9c90
Sara <parakeet_22@yahoo.com>
2023-01-24 20:44:59 -0500

    add description

--- bloom/src/components/Plant1.js
+++ bloom/src/components/Plant1.js
@@ -7,10 +7,11 @@ import two from "../images/2.png";
 import three from "../images/3.png";
 import four from "../images/4.png";
 
-
+//Component swaps through images on click
 export default function Plant1() {
   const [image, setImage] = useState(0);
   const images = [one, two, three, four];
+  //fuction to loop the image
   const loopImage = function () {
     setImage(image === images.length - 1 ? 0 : image + 1);
   };
