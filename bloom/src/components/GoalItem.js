2c5c08e437514377045307697c11cc0b68cda4bd
Sara <parakeet_22@yahoo.com>
2023-03-03 21:46:06 -0500

    add basic Task Item

--- bloom/src/components/GoalItem.js
+++ bloom/src/components/GoalItem.js
@@ -1,10 +1,13 @@
 import react, {useState} from 'react';
 import './GoalItem.scss';
+import TaskItem from './TaskItem';
 
 export default function GoalItem(props) {
   return (
     <div className="Goal Item"> 
 <h2 className = "Goal Name"> GOAL NAME PlACEHOLDER in GoalItem</h2>
+<TaskItem />
+<TaskItem />
 </div>
   );
 }
---
+++ bloom/src/components/TaskItem.js
@@ -0,0 +1,10 @@
+import react, {useState} from 'react';
+import './TaskItem.scss';
+
+export default function TaskItem(props) {
+  return (
+    <div className="TaskItem"> 
+<h3 className = "Task"> PlaceHolder Task</h3>
+</div>
+  );
+}
