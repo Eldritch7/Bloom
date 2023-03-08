459f6230d766eb8db4adb3d69e6490bf9b537065
Sara <parakeet_22@yahoo.com>
2023-03-07 23:32:59 -0500

    add classnames module and basic button component

---
+++ bloom/src/components/Button.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./Button.scss";
+
+import classNames from "classnames";
+
+export default function Button(props) {
+  let onClick;
+
+  let buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+}
--- bloom/src/components/GoalItem.js
+++ bloom/src/components/GoalItem.js
@@ -1,11 +1,14 @@
 import react, {useState} from 'react';
+import Button from './Button';
 import './GoalItem.scss';
+
 import TaskItem from './TaskItem';
 
 export default function GoalItem(props) {
   return (
     <div className="Goal Item"> 
 <h2 className = "Goal Name"> GOAL NAME PlACEHOLDER in GoalItem</h2>
+<Button />
 <TaskItem />
 <TaskItem />
 </div>
